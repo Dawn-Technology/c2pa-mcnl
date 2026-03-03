496b8273519b81fda1e7951165578762f75e577b
Rob van Dam <rob.van.dam@dockbite.nl>
2026-03-03 09:11:37 +0100

    feat: enhance file upload component accessibility and UI interactions

--- apps/verify-webapp/src/app/app.spec.ts
+++ apps/verify-webapp/src/app/app.spec.ts
@@ -1,16 +1,18 @@
 import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [provideRouter([])],
     }).compileComponents();
   });
 
   it('should render', async () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
+    // const compiled = fixture.nativeElement as HTMLElement;
   });
 });
