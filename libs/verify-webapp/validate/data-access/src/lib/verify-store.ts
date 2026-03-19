68be52485f66e95f78be2667e96549dc90bce563
Rob van Dam <rob.van.dam@dockbite.nl>
2026-03-19 11:19:43 +0100

    feat(verify-webapp): improve and cleanup verify store for reading the manifest

--- libs/verify-webapp/validate/data-access/src/lib/verify-store.ts
+++ libs/verify-webapp/validate/data-access/src/lib/verify-store.ts
@@ -12,10 +12,13 @@ import {
   ManifestStore,
   ThumbnailAssertion,
   ThumbnailType,
+  ValidationError,
+  ValidationResult,
 } from '@dockbite/c2pa-ts/manifest';
 import { SuperBox } from '@dockbite/c2pa-ts/jumbf';
-import { createAsset } from '@dockbite/c2pa-ts/asset';
+import { Asset, createAsset } from '@dockbite/c2pa-ts/asset';
 import { formatDate } from '@angular/common';
+import { isImageMimeType } from '@c2pa-mcnl/shared/utils/helpers';
 
 type VerifyStoreState = {
   isLoading: boolean;
@@ -23,18 +26,25 @@ type VerifyStoreState = {
   file: File | null;
   fileDataUrl: string | null;
 
+  asset: Asset | null;
   manifestStore: ManifestStore | null;
+  manifestStoreReadValidationError?: ValidationError;
+  manifestsThumbnailUrls: Map<string, string> | null; // label → blob URL
+
   activeManifest: Manifest | null;
-  manifestThumbnailUrls: Map<string, string>; // label → blob URL
+  activeManifestValidationResult?: ValidationResult;
+
+  error?: unknown;
 };
 
 const initialState: VerifyStoreState = {
   isLoading: false,
   file: null,
   fileDataUrl: null,
+  asset: null,
   manifestStore: null,
   activeManifest: null,
-  manifestThumbnailUrls: new Map(),
+  manifestsThumbnailUrls: null,
 };
 
 export const VerifyStore = signalStore(
@@ -42,9 +52,7 @@ export const VerifyStore = signalStore(
   withState(initialState),
   withComputed(({ file, manifestStore }, localeId = inject(LOCALE_ID)) => ({
     hasFile: computed(() => !!file()),
-
     hasManifests: computed(() => manifestStore()?.manifests.length),
-
     fileDate: computed(() => {
       const lastModified = file()?.lastModified;
       if (!lastModified) {
@@ -53,105 +61,130 @@ export const VerifyStore = signalStore(
 
       return formatDate(lastModified, 'dd MMMM yyyy', localeId);
     }),
-
-    /**
-     * Reversed ordering of the manifests array as we want to show the
-     * last value as the first in the view.
-     */
     manifestsReversed: computed(() =>
       [...(manifestStore()?.manifests ?? [])].reverse(),
     ),
   })),
   withMethods((store) => ({
     async setFile(file: File | null) {
-      if (!file) {
-        this._revokeCurrentFileDataUrl();
-        patchState(store, () => ({
-          file,
-        }));
-        return;
-      }
-
-      const prevFileDataUrl = store.fileDataUrl();
-      if (prevFileDataUrl) {
-        URL.revokeObjectURL(prevFileDataUrl);
-      }
-
-      const fileDataUrl = URL.createObjectURL(file);
-
-      const prevManifestThumbnailUrls = store.manifestThumbnailUrls();
-      for (const url of prevManifestThumbnailUrls.values()) {
-        URL.revokeObjectURL(url);
-      }
-
       patchState(store, () => ({
         isLoading: true,
-        fileDataUrl,
-        file,
       }));
 
-      // todo error handling
-      const asset = await createAsset(file);
-      const jumbfBytes = await asset.getManifestJUMBF();
+      this._revokeCurrentFileDataUrl();
+      this._revokeCurrentManifestThumbnailUrls();
 
-      if (jumbfBytes) {
-        const manifestStore = ManifestStore.read(
-          SuperBox.fromBuffer(jumbfBytes),
-        );
-        const activeManifest = manifestStore.getActiveManifest();
+      if (!file) {
+        console.debug('No file selected, resetting state');
+        return this._resetState();
+      }
 
-        // todo move to separate method
-        const manifestThumbnailUrls = new Map<string, string>();
-        for (const manifest of manifestStore?.manifests || []) {
-          if (!manifest.label) {
-            continue;
+      const fileDataUrl = this._createFileDataUrl(file);
+
+      patchState(store, () => ({
+        fileDataUrl,
+        file,
+        manifestsThumbnailUrls: null,
+      }));
+
+      try {
+        const asset = await createAsset(file);
+        const jumbfBytes = await asset.getManifestJUMBF();
+
+        /* no manifest found */
+        if (!jumbfBytes) {
+          return patchState(store, {
+            isLoading: false,
+            manifestStore: null,
+            activeManifest: null,
+          });
+        }
+
+        let manifestStore: ManifestStore;
+        try {
+          manifestStore = ManifestStore.read(SuperBox.fromBuffer(jumbfBytes));
+        } catch (error) {
+          if (error instanceof ValidationError) {
+            console.error('Manifest validation error:', error);
+            return patchState(store, () => ({
+              isLoading: false,
+              manifestStoreReadValidationError: error,
+            }));
+          } else {
+            console.error('An unexpected error occurred.', String(error));
+            return patchState(store, () => ({
+              isLoading: false,
+              error,
+            }));
           }
+        }
 
-          const thumbnails =
-            manifest.assertions?.getThumbnailAssertions() as ThumbnailAssertion[];
+        const manifestsThumbnailUrls = this._createManifestsThumbnailUrls(
+          manifestStore.manifests,
+        );
 
-          const claimThumbnail = thumbnails?.find(
-            (thumbnail) => thumbnail.thumbnailType === ThumbnailType.Claim,
-          );
-
-          if (claimThumbnail?.content) {
-            const thumbnailDataUrl = URL.createObjectURL(
-              new Blob([new Uint8Array(claimThumbnail.content)], {
-                type: claimThumbnail.mimeType,
-              }),
-            );
-
-            manifestThumbnailUrls.set(manifest.label, thumbnailDataUrl);
+        let activeManifestValidationResult: ValidationResult;
+        try {
+          activeManifestValidationResult = await manifestStore.validate(asset);
+          console.log(activeManifestValidationResult);
+        } catch (error) {
+          if (error instanceof ValidationError) {
+            console.error('Manifest validation error:', error);
+            return patchState(store, () => ({
+              isLoading: false,
+              manifestStoreReadValidationError: error,
+            }));
+          } else {
+            console.error('An unexpected error occurred.', String(error));
+            return patchState(store, () => ({
+              isLoading: false,
+              error,
+            }));
           }
         }
 
         patchState(store, () => ({
           isLoading: false,
+          asset,
           manifestStore,
-          activeManifest,
-          manifestThumbnailUrls,
+          manifestsThumbnailUrls,
+          activeManifest: manifestStore.getActiveManifest(),
+          activeManifestValidationResult,
+        }));
+      } catch (error) {
+        patchState(store, () => ({
+          error,
         }));
       }
     },
-    setActiveManifest(label?: string) {
+
+    async setActiveManifest(label?: string) {
+      const asset = store.asset();
+      const manifestStore = store.manifestStore();
+
+      if (!asset || !manifestStore || !label) {
+        return;
+      }
+
       patchState(store, () => ({
         isLoading: true,
       }));
 
-      let activeManifest: Manifest | null;
-      if (label) {
-        activeManifest =
-          store?.manifestStore()?.getManifestByLabel(label) || null;
-      }
+      const activeManifest = manifestStore.getManifestByLabel(label);
+      const activeManifestValidationResult =
+        await activeManifest?.validate(asset);
 
       patchState(store, () => ({
-        activeManifest,
         isLoading: false,
+        activeManifest,
+        activeManifestValidationResult,
       }));
     },
 
-    getManifestThumbnailDataUrl(label?: string) {
-      return label ? (store.manifestThumbnailUrls().get(label) ?? null) : null;
+    getManifestsThumbnailDataUrl(label?: string) {
+      return label
+        ? (store?.manifestsThumbnailUrls()?.get(label) ?? null)
+        : null;
     },
 
     isActiveManifest(label?: string) {
@@ -159,26 +192,58 @@ export const VerifyStore = signalStore(
     },
 
     _createFileDataUrl(file: File) {
-      const prevFileDataUrl = store.fileDataUrl();
-      const fileDataUrl = URL.createObjectURL(file);
-
-      patchState(store, () => ({
-        fileDataUrl,
-      }));
-
-      if (prevFileDataUrl) {
-        URL.revokeObjectURL(prevFileDataUrl);
+      if (!isImageMimeType(file)) {
+        return null;
       }
+
+      return URL.createObjectURL(file);
     },
+
     _revokeCurrentFileDataUrl() {
       const fileDataUrl = store.fileDataUrl();
       if (fileDataUrl) {
         URL.revokeObjectURL(fileDataUrl);
       }
+    },
 
-      patchState(store, () => ({
-        fileDataUrl: null,
-      }));
+    _createManifestsThumbnailUrls(
+      manifests: Manifest[] = [],
+      manifestThumbnailUrls = new Map<string, string>(),
+    ) {
+      for (const manifest of manifests) {
+        if (!manifest.label) {
+          continue;
+        }
+
+        const thumbnails =
+          manifest.assertions?.getThumbnailAssertions() as ThumbnailAssertion[];
+
+        const claimThumbnail = thumbnails?.find(
+          (thumbnail) => thumbnail.thumbnailType === ThumbnailType.Claim,
+        );
+
+        if (claimThumbnail?.content) {
+          const thumbnailDataUrl = URL.createObjectURL(
+            new Blob([new Uint8Array(claimThumbnail.content)], {
+              type: claimThumbnail.mimeType,
+            }),
+          );
+
+          manifestThumbnailUrls.set(manifest.label, thumbnailDataUrl);
+        }
+      }
+
+      return manifestThumbnailUrls;
+    },
+
+    _revokeCurrentManifestThumbnailUrls() {
+      for (const url of store.manifestsThumbnailUrls()?.values() || []) {
+        URL.revokeObjectURL(url);
+      }
+    },
+
+    _resetState() {
+      patchState(store, initialState);
     },
   })),
   withHooks({
