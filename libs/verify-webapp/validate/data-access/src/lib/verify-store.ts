7cf9ff118a0297b3f0951e6f2b6a3541c114746a
Rob van Dam <rob.van.dam@dockbite.nl>
2026-03-20 09:31:09 +0100

    feat(verify-webapp): validate manifests and wip for showing action assertions

--- libs/verify-webapp/validate/data-access/src/lib/verify-store.ts
+++ libs/verify-webapp/validate/data-access/src/lib/verify-store.ts
@@ -14,6 +14,7 @@ import {
   ThumbnailType,
   ValidationError,
   ValidationResult,
+  ValidationStatusCode,
 } from '@dockbite/c2pa-ts/manifest';
 import { SuperBox } from '@dockbite/c2pa-ts/jumbf';
 import { Asset, createAsset } from '@dockbite/c2pa-ts/asset';
@@ -29,10 +30,9 @@ type VerifyStoreState = {
   asset: Asset | null;
   manifestStore: ManifestStore | null;
   manifestStoreReadValidationError?: ValidationError;
-  manifestsThumbnailUrls: Map<string, string> | null; // label → blob URL
-
+  manifestThumbnailUrls: Map<string, string> | null; // label → blob URL
+  manifestValidationResults: Map<string, ValidationResult> | null;
   activeManifest: Manifest | null;
-  activeManifestValidationResult?: ValidationResult;
 
   error?: unknown;
 };
@@ -43,28 +43,38 @@ const initialState: VerifyStoreState = {
   fileDataUrl: null,
   asset: null,
   manifestStore: null,
+  manifestThumbnailUrls: null,
+  manifestValidationResults: null,
   activeManifest: null,
-  manifestsThumbnailUrls: null,
 };
 
 export const VerifyStore = signalStore(
   { providedIn: 'root' },
   withState(initialState),
-  withComputed(({ file, manifestStore }, localeId = inject(LOCALE_ID)) => ({
-    hasFile: computed(() => !!file()),
-    hasManifests: computed(() => manifestStore()?.manifests.length),
-    fileDate: computed(() => {
-      const lastModified = file()?.lastModified;
-      if (!lastModified) {
-        return '—';
-      }
+  withComputed(
+    (
+      { file, manifestStore, activeManifest },
+      localeId = inject(LOCALE_ID),
+    ) => ({
+      hasFile: computed(() => !!file()),
+      hasManifests: computed(() => manifestStore()?.manifests.length),
+      fileDate: computed(() => {
+        const lastModified = file()?.lastModified;
+        if (!lastModified) {
+          return '—';
+        }
 
-      return formatDate(lastModified, 'dd MMMM yyyy', localeId);
+        return formatDate(lastModified, 'dd MMMM yyyy', localeId);
+      }),
+      manifestsReversed: computed(() =>
+        [...(manifestStore()?.manifests ?? [])].reverse(),
+      ),
+
+      getActionAssertions: computed(
+        () => activeManifest()?.assertions?.getActionAssertions() ?? [],
+      ),
     }),
-    manifestsReversed: computed(() =>
-      [...(manifestStore()?.manifests ?? [])].reverse(),
-    ),
-  })),
+  ),
   withMethods((store) => ({
     async setFile(file: File | null) {
       patchState(store, () => ({
@@ -84,7 +94,7 @@ export const VerifyStore = signalStore(
       patchState(store, () => ({
         fileDataUrl,
         file,
-        manifestsThumbnailUrls: null,
+        manifestThumbnailUrls: null,
       }));
 
       try {
@@ -101,8 +111,14 @@ export const VerifyStore = signalStore(
         }
 
         let manifestStore: ManifestStore;
+        let manifestValidationResults: VerifyStoreState['manifestValidationResults'];
         try {
           manifestStore = ManifestStore.read(SuperBox.fromBuffer(jumbfBytes));
+
+          manifestValidationResults = await this._validateManifests(
+            manifestStore,
+            asset,
+          );
         } catch (error) {
           if (error instanceof ValidationError) {
             console.error('Manifest validation error:', error);
@@ -111,7 +127,7 @@ export const VerifyStore = signalStore(
               manifestStoreReadValidationError: error,
             }));
           } else {
-            console.error('An unexpected error occurred.', String(error));
+            console.error('Manifest validation unknown error.', String(error));
             return patchState(store, () => ({
               isLoading: false,
               error,
@@ -119,39 +135,28 @@ export const VerifyStore = signalStore(
           }
         }
 
-        const manifestsThumbnailUrls = this._createManifestsThumbnailUrls(
+        const manifestThumbnailUrls = this._createManifestThumbnailUrls(
           manifestStore.manifests,
         );
 
-        let activeManifestValidationResult: ValidationResult;
-        try {
-          activeManifestValidationResult = await manifestStore.validate(asset);
-          console.log(activeManifestValidationResult);
-        } catch (error) {
-          if (error instanceof ValidationError) {
-            console.error('Manifest validation error:', error);
-            return patchState(store, () => ({
-              isLoading: false,
-              manifestStoreReadValidationError: error,
-            }));
-          } else {
-            console.error('An unexpected error occurred.', String(error));
-            return patchState(store, () => ({
-              isLoading: false,
-              error,
-            }));
-          }
-        }
+        const activeManifest = manifestStore.getActiveManifest();
 
+        console.debug('active manifest:', activeManifest);
+        console.debug(
+          'manifests validationr results:',
+          manifestValidationResults,
+        );
         patchState(store, () => ({
           isLoading: false,
           asset,
           manifestStore,
-          manifestsThumbnailUrls,
-          activeManifest: manifestStore.getActiveManifest(),
-          activeManifestValidationResult,
+          manifestThumbnailUrls,
+          manifestValidationResults,
+          activeManifest,
         }));
       } catch (error) {
+        const message = error instanceof Error ? error.message : String(error);
+        console.error('An unexpected error occurred.', message);
         patchState(store, () => ({
           error,
         }));
@@ -171,19 +176,16 @@ export const VerifyStore = signalStore(
       }));
 
       const activeManifest = manifestStore.getManifestByLabel(label);
-      const activeManifestValidationResult =
-        await activeManifest?.validate(asset);
 
       patchState(store, () => ({
         isLoading: false,
         activeManifest,
-        activeManifestValidationResult,
       }));
     },
 
     getManifestsThumbnailDataUrl(label?: string) {
       return label
-        ? (store?.manifestsThumbnailUrls()?.get(label) ?? null)
+        ? (store?.manifestThumbnailUrls()?.get(label) ?? null)
         : null;
     },
 
@@ -206,7 +208,36 @@ export const VerifyStore = signalStore(
       }
     },
 
-    _createManifestsThumbnailUrls(
+    async _validateManifests(manifestStore: ManifestStore, asset: Asset) {
+      const validationResults: VerifyStoreState['manifestValidationResults'] =
+        new Map();
+
+      const activeManifest = manifestStore.getActiveManifest();
+      if (!activeManifest) {
+        throw new ValidationError(
+          ValidationStatusCode.ClaimCBORInvalid,
+          manifestStore.sourceBox,
+          'Active manifest is missing',
+        );
+      }
+
+      for (const manifest of manifestStore.manifests) {
+        const label = manifest.label;
+        if (!label) {
+          throw new ValidationError(
+            ValidationStatusCode.GeneralError,
+            manifestStore.sourceBox,
+            'The manifest is missing a label',
+          );
+        }
+
+        validationResults.set(label, await manifest.validate(asset));
+      }
+
+      return validationResults;
+    },
+
+    _createManifestThumbnailUrls(
       manifests: Manifest[] = [],
       manifestThumbnailUrls = new Map<string, string>(),
     ) {
@@ -237,7 +268,7 @@ export const VerifyStore = signalStore(
     },
 
     _revokeCurrentManifestThumbnailUrls() {
-      for (const url of store.manifestsThumbnailUrls()?.values() || []) {
+      for (const url of store.manifestThumbnailUrls()?.values() || []) {
         URL.revokeObjectURL(url);
       }
     },
