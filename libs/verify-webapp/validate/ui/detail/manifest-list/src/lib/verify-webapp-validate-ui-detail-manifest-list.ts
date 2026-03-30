12ec0288e8ff9bc5414b68ce771dfd5c9a9e1ee8
Bert Lobbezoo <bert.lobbezoo@dawn.tech>
2026-03-30 17:36:55 +0200

    WIP: Dynamic content for C2PA manifest
    # Conflicts:
    #	libs/verify-webapp/validate/ui/detail/manifest-info/src/lib/verify-webapp-validate-ui-detail-manifest-info.html
    #	libs/verify-webapp/validate/ui/detail/manifest-list/src/lib/verify-webapp-validate-ui-detail-manifest-list.html

--- libs/verify-webapp/validate/ui/detail/manifest-list/src/lib/verify-webapp-validate-ui-detail-manifest-list.ts
+++ libs/verify-webapp/validate/ui/detail/manifest-list/src/lib/verify-webapp-validate-ui-detail-manifest-list.ts
@@ -2,10 +2,15 @@ import { Component, inject } from '@angular/core';
 import { VerifyStore } from '@c2pa-mcnl/verify-webapp/validate/data-access';
 import { NgClass } from '@angular/common';
 import { VerifyWebappValidateUiValidationBadge } from '@c2pa-mcnl/verify-webapp/validate/ui/validation-badge';
+import { VerifyWebappValidateUiFileCard } from '@c2pa-mcnl/verify-webapp/validate/ui/file-card';
 
 @Component({
   selector: 'lib-verify-webapp-validate-ui-detail-manifest-list',
-  imports: [NgClass, VerifyWebappValidateUiValidationBadge],
+  imports: [
+    NgClass,
+    VerifyWebappValidateUiValidationBadge,
+    VerifyWebappValidateUiFileCard,
+  ],
   templateUrl: './verify-webapp-validate-ui-detail-manifest-list.html',
   styleUrl: './verify-webapp-validate-ui-detail-manifest-list.css',
 })
