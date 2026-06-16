5d0efdea3051be5103e163d7c568409b291b53f1
bertlobbezoo <bert.lobbezoo@dawn.tech>
2026-06-16 10:48:25 +0200

    Valid, untrusted and invalid badge (#30)

--- libs/verify-webapp/validate/ui/validation-badge/src/lib/verify-webapp-validate-ui-validation-badge.ts
+++ libs/verify-webapp/validate/ui/validation-badge/src/lib/verify-webapp-validate-ui-validation-badge.ts
@@ -1,12 +1,15 @@
 import { Component, input } from '@angular/core';
 
+export type ValidationBadgeState = 'valid' | 'invalid' | 'untrusted';
+
 @Component({
   selector: 'lib-verify-webapp-validate-ui-validation-badge',
   templateUrl: './verify-webapp-validate-ui-validation-badge.html',
   styleUrl: './verify-webapp-validate-ui-validation-badge.css',
 })
 export class VerifyWebappValidateUiValidationBadge {
-  valid = input.required<boolean>();
+  state = input.required<ValidationBadgeState>();
   textValid = input<string>('');
-  textInvalid = input('Niet herkenbaar');
+  textInvalid = input('Gemanipuleerd');
+  textUntrusted = input('Niet vertrouwd');
 }
