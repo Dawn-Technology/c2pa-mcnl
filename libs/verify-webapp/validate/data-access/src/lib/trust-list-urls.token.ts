9f2112374826a6c714a3a09ec4e3f29d6a6d6988
Rob van Dam <rob.van.dam@dockbite.nl>
2026-06-16 09:58:38 +0200

    chore: enable back the trust urls

--- libs/verify-webapp/validate/data-access/src/lib/trust-list-urls.token.ts
+++ libs/verify-webapp/validate/data-access/src/lib/trust-list-urls.token.ts
@@ -6,8 +6,8 @@ export const VERIFY_TRUST_LIST_URLS = new InjectionToken<readonly string[]>(
     providedIn: 'root',
     factory: () => [
       'https://raw.githubusercontent.com/Dawn-Technology/c2pa-mcnl-trust-list/refs/heads/main/trust-list/C2PA-TRUST-LIST.pem',
-      // 'https://raw.githubusercontent.com/c2pa-org/conformance-public/refs/heads/main/trust-list/C2PA-TRUST-LIST.pem',
-      // 'https://verify.contentauthenticity.org/trust/anchors.pem',
+      'https://raw.githubusercontent.com/c2pa-org/conformance-public/refs/heads/main/trust-list/C2PA-TRUST-LIST.pem',
+      'https://verify.contentauthenticity.org/trust/anchors.pem',
     ],
   },
 );
